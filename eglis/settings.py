0e7d7d6094f8a9b459db61b51360a1b80949d526
Manulynx <lynxiyo@gmail.com>
2025-08-15 16:04:37 -0400

    Debug true

--- eglis/settings.py
+++ eglis/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z$sk-v#*23+1y2v1_yvgy@olb%v(l$%(l+cbkcgzp7qtks5+84'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['carloseglis.pythonanywhere.com']
 
