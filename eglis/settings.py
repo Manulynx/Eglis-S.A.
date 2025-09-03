115bac14f5f665a22820598c06ec3849564c4ac8
Manulynx <lynxiyo@gmail.com>
2025-09-03 08:45:20 -0400

    otro server

--- eglis/settings.py
+++ eglis/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-z$sk-v#*23+1y2v1_yvgy@olb%v(l$%(l+cbkcgzp7qtks5+84
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['carloseglis.pythonanywhere.com']
+ALLOWED_HOSTS = ['carloseglis.pythonanywhere.com',
+                 'eglissa.pythonanywhere.com']
 
 
 # Application definition
